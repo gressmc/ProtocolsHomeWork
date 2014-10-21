70c8cc2e6427c693fc8b28151771a0dcddae28d3
gressmc <gressmc@iMac.local>
2014-10-21 19:14:27 +0400

    Initial Commit

---
+++ ProtocolsHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtocolsHomeWork
+//
+//  Created by gressmc on 21/10/14.
+//  Copyright (c) 2014 gressmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
